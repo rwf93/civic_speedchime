c352a4cab9170109ef0f6c72046108ef11afbcdf
rwf93 <22036195+rwf93@users.noreply.github.com>
2024-08-03 18:59:39 -0700

    do speed chime

--- main.c
+++ main.c
@@ -11,6 +11,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <pico/multicore.h>
+
 #include "hardware/timer.h"
 #include "pio_reader.pio.h"
 
@@ -23,8 +25,28 @@ static uint32_t now = 0;
 static uint32_t gap = 0;
 static uint32_t last = 0;
 
-static uint32_t last_chime = 0;
-static uint32_t gap_chime = 0;
+void core1_sio_irq() {
+	printf("Getting chime signal\n");
+
+	while(multicore_fifo_rvalid()) multicore_fifo_pop_blocking();
+	multicore_fifo_clear_irq();
+
+	gpio_put(SPEED_CHIME_PIN, true);
+	busy_wait_us_32(250000);
+	gpio_put(SPEED_CHIME_PIN, false);
+	busy_wait_us_32(1e6);
+
+	gpio_put(SPEED_CHIME_PIN, true);
+	busy_wait_us_32(250000);
+	gpio_put(SPEED_CHIME_PIN, false);
+	busy_wait_us_32(1e6);
+}
+
+void core1_entry() {
+	irq_set_exclusive_handler(SIO_IRQ_PROC1, core1_sio_irq);
+    irq_set_enabled(SIO_IRQ_PROC1, true);
+	while(true) tight_loop_contents();
+}
 
 // 100: 7000-8000 range
 // 30-40: last read was 23000 - 23400
@@ -32,23 +54,30 @@ void gpio_callback(uint gpio, uint32_t events) {
 	now = time_us_32();
 	gap = now - last;
 	last = now;
-	if(gap > 4097) {
-		//printf("%lu\n", gap);
-		if(gap <= 7300) {
-			gap_chime = last_chime;
-			last = now;
 
-			speed_sensor_enable = false;
-			gpio_put(SPEED_CHIME_PIN, true);
+	if(gap > 8000) {
+		printf("%lu\n", gap);
+		if(gap <= 18500)
+			multicore_fifo_push_blocking(0);
+	}
+
+	/*
+	if(gap > 4097) {
+		printf("%lu\n", gap);
+
+		if(gap <= 23500) {
 			sleep_ms(250);
-			gpio_put(SPEED_CHIME_PIN, false);
+			gpio_put(SPEED_CHIME_PIN, true);
 			sleep_ms(1000);
-			speed_sensor_enable = true;
+			gpio_put(SPEED_CHIME_PIN, false);
+			sleep_ms(250);
+			gpio_put(SPEED_CHIME_PIN, true);
+			sleep_ms(1000);
+			gpio_put(SPEED_CHIME_PIN, false);
 		}
 
-		//printf("%lu\n", gap_chime);
-
-		/*
+		if(gap >= 7000 && gap <= 8000)
+			printf("hitting 90-100?\n");
 		if(gap >= 12000 && gap <= 13200)
 			printf("hitting 60\n");
 		if(gap >= 15200 && gap <= 15500)
@@ -57,8 +86,9 @@ void gpio_callback(uint gpio, uint32_t events) {
 			printf("hitting 40\n");
 		if(gap >= 23100 && gap <= 23500)
 			printf("hitting 30\n");
-		*/
+
 	}
+	*/
 }
 
 int main() {
@@ -76,9 +106,13 @@ int main() {
 	}
 	*/
 
+
 	now = time_us_32();
-	gpio_set_irq_enabled_with_callback(SPEED_SENSOR_PIN, GPIO_IRQ_EDGE_RISE, &speed_sensor_enable, &gpio_callback);
-	while(1);
+	gpio_set_irq_enabled_with_callback(SPEED_SENSOR_PIN, GPIO_IRQ_EDGE_RISE, true, &gpio_callback);
+
+	multicore_launch_core1(core1_entry);
+
+	while(1) {}
 
 	return 0;
 }
